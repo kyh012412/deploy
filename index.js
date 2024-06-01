21ec4a0ec46fc81be4ca99c22f05f03d283f62c6
kyh012412 <kyh0124.12@gmail.com>
2024-06-01 22:19:24 +0900

    배포 테스트

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: true,
+  })
+);
+
+app.listen(3000, (req, res) => {
+  console.log('server listening in port 3000');
+});
+
+app.get('', (req, res) => {
+  res.send('hi');
+});
